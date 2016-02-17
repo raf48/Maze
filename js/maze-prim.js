f1f71fb46d99a346f9c3081d6c95e04d623a26e3
raf48 <rafalhot48@gmail.com>
2016-02-18 01:35:53 +0200

    code refactoring

--- js/maze-prim.js
+++ js/maze-prim.js
@@ -1,19 +1,68 @@
-/* An implementation of randomized Prim's algorithm for 2-d maze generation */
-function generateP(width, height, matrix, startX, startY) {
+/* An implementation of Prim's algorithm for 2-d maze generation */
+function generateP(width, height, startX, startY) {
 
   var x_coords_out = [],  /* Array used to save generated x-axis coordinates */
-      y_coords_out = [];  /* Array used to save generated y-axis coordinates */
+      y_coords_out = [],  /* Array used to save generated y-axis coordinates */
+      matrix = Array.matrix(width, height, 0), /* 2-dimentional array */
+      heap,
+      w,
+      tmpCell;
+
+  /* Min-heap binary tree */
+  function MinHeap() {
+    /* Root element is null */
+    this.data = [null];
+  }
+
+  MinHeap.prototype = {
+    insert: function(el) {
+      this.bubble(this.data.push(el) - 1);
+    },
+    remove: function() {
+      var r = this.data[1];
+      this.data[1] = this.data.pop();
+      this.sink(1);
+      return r;
+    },
+    bubble: function(i) {
+      var parentIndex = Math.floor(i/2);
+      if (this.data[parentIndex] && this.data[parentIndex].weight > this.data[i].weight) {
+        this.swap(i, parentIndex);
+        this.bubble(parentIndex);
+      }
+    },
+    swap: function(i, j) {
+      var tmp = this.data[i];
+      this.data[i] = this.data[j];
+      this.data[j] = tmp;
+    },
+    sink: function(i) {
+      if (!this.data[i*2] || !this.data[i*2 + 1]) return;
+      var leftSmaller = this.data[i*2].weight < this.data[i*2 + 1].weight,
+          childIndex = leftSmaller ? i*2 : i*2 + 1;
+      if (this.data[i].weight > this.data[childIndex].weight) {
+        this.swap(i, childIndex);
+        if (childIndex*2 + 1 < this.data.length) {
+          this.sink(childIndex);
+        }
+      }
+    },
+    childExists: function() {
+      return !!this.data[2];
+    }
+  };
 
   function Cell(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  function Wall(x, y, left, right) {
+  function Wall(x, y, left, right, weight) {
     this.x = x;
     this.y = y;
     this.left = left;
     this.right = right;
+    this.weight = weight;
   }
 
   function createCells(m) {
@@ -31,14 +80,14 @@ function generateP(width, height, matrix, startX, startY) {
     /* Create horizontal walls */
     for (i = 1; i < m.length - 1; i+=2) {
       for (j = 0; j < m[i].length; j+=2) {
-        m[i][j] = new Wall(i, j, m[i - 1][j], m[i + 1][j]);
+        m[i][j] = new Wall(i, j, m[i - 1][j], m[i + 1][j], Math.random());
       }
     }
 
     /* Create vertical walls */
     for (i = 0; i < m.length; i+=2) {
       for (j = 1; j < m[i].length - 1; j+=2) {
-        m[i][j] = new Wall(i, j, m[i][j - 1], m[i][j + 1]);
+        m[i][j] = new Wall(i, j, m[i][j - 1], m[i][j + 1], Math.random());
       }
     }
   }
@@ -46,46 +95,44 @@ function generateP(width, height, matrix, startX, startY) {
   /* Add walls of a given cell to the wall list */
   function addWalls(i, j) {
     if (typeof matrix[i + 1] !== 'undefined' && typeof matrix[i + 1][j] !== 'undefined') {
-      wallList.push(matrix[i + 1][j]);
+      heap.insert(matrix[i + 1][j]);
     }
     if (typeof matrix[i - 1] !== 'undefined' && typeof matrix[i - 1][j] !== 'undefined') {
-      wallList.push(matrix[i - 1][j]);
+      heap.insert(matrix[i - 1][j]);
     }
     if (typeof matrix[i] !== 'undefined' && typeof matrix[i][j + 1] !== 'undefined') {
-      wallList.push(matrix[i][j + 1]);
+      heap.insert(matrix[i][j + 1]);
     }
     if (typeof matrix[i] !== 'undefined' && typeof matrix[i][j - 1] !== 'undefined') {
-      wallList.push(matrix[i][j - 1]);
+      heap.insert(matrix[i][j - 1]);
     }
   }
 
-  var wallList = [], i = startX, j = startY, w, tmpCell;
+  heap = new MinHeap();
 
   createCells(matrix);
   createWalls(matrix);
 
   /* Mark first cell as part of the maze, push its coordinates to the output stack
    * and add it's walls to the wall list */
-  matrix[i][j].visited = true;
-  x_coords_out.push(j);
-  y_coords_out.push(i);
-  addWalls(i, j);
+  matrix[startX][startY].visited = true;
+  x_coords_out.push(startX);
+  y_coords_out.push(startY);
+  addWalls(startX, startY);
 
   /* While there are walls in the list */
-  while (wallList.length) {
+  while (heap.childExists()) {
     /* Pick a random wall from the list */
-    w = wallList.splice(Math.floor(Math.random() * wallList.length), 1)[0];
+    w = heap.remove();
     /* Find if the opposite cell has not yet been visited,
      * add it's coordinates to the output stack,
      * add neighboring walls of the cell to the wall list. */
-    if (w !== 0 && (!w.left.visited || !w.right.visited)) {
+    if (w && (!w.left.visited || !w.right.visited)) {
       tmpCell = (!w.left.visited) ? w.left : w.right;
       tmpCell.visited = true;
-      x_coords_out.push(w.y, tmpCell.y);
-      y_coords_out.push(w.x, tmpCell.x);
-      i = tmpCell.x;
-      j = tmpCell.y;
-      addWalls(i, j);
+      x_coords_out.push(w.x, tmpCell.x);
+      y_coords_out.push(w.y, tmpCell.y);
+      addWalls(tmpCell.x, tmpCell.y);
     }
   }
 
